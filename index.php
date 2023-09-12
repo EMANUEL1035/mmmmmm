a5ff6536a55ef77c0280649fdb6dbaa36671c8ab
EMANUEL1035 <133671987+EMANUEL1035@users.noreply.github.com>
2023-09-12 14:14:53 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="description" content="">
+        <meta name="robots" content="index,follow">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Operadores Aritmeticos</title>
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+        <p class="text">HOLA MUNDO</p>
+    </body>
+</html>
